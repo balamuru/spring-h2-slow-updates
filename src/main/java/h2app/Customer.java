061d972954ea0be5fed192d987db535c1ac0d960
balamuru <vybe3142@gmail.com>
2014-05-15 18:58:30 -0500

    initial

--- src/main/java/h2app/Customer.java
+++ src/main/java/h2app/Customer.java
@@ -9,8 +9,9 @@ public class Customer {
     public Customer(long id, String firstName, String lastName, boolean valid) {
         this.id = id;
         this.firstName = firstName;
-        this.lastName = lastName;
         this.valid = valid;
+        this.lastName = lastName;
+
     }
 
     public long getId() {
